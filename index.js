15d4f1fd4c59e14618842d59ac92c629a7afb784
trang <hd.oang.ong@gmail.com>
2019-12-07 17:21:01 +0700

    change Cat require

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 var Dog=require('./Dog');
-var Dog=require('./Cat');
+var Cat=require('./Cat');
 var dog=new Dog('cún cưng');
 dog.sayhi();
 var cat= new Cat ('mèo thúi');
 cat.sayhi();
+dog.eat(cat);
+console.log(cat.name);
