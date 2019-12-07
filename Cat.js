aac190a299e038fc548dd569250de58702fc04f1
trang <hd.oang.ong@gmail.com>
2019-12-07 18:06:51 +0700

    add Mouse.js

--- Cat.js
+++ Cat.js
@@ -1,10 +1,18 @@
+var mouse=require('./Mouse');
 var chalk =require('chalk')
 function  Cat(name){
     this.name=name;
     this.stomach=[];
 }
-Cat.prototype.eat=function(mouse){
-    this.stomach.push(mouse);
+Cat.prototype.eat=function(animal){
+    if(animal instanceof mouse){
+        this.stomach.push(animal);
+    }
+    else{
+        throw new Error('Cat can only eat Mouse');
+        
+    }
+    
 }
 Cat.prototype.sayhi=function(){
     console.log('xin chào tui là con Cat, tên của tui là: '+chalk.red(this.name));
--- index.js
+++ index.js
@@ -1,8 +1,19 @@
 var Dog=require('./Dog');
 var Cat=require('./Cat');
+var Mouse=require('./Mouse');
+
 var dog=new Dog('cún cưng');
 dog.sayhi();
+
 var cat= new Cat ('mèo thúi');
 cat.sayhi();
+
+var mouse=new Mouse('Chuột bạch');
+mouse.sayhi();
+
 dog.eat(cat);
+cat.eat(mouse);
+cat.eat(dog);
+
 console.log(dog.stomach);
+console.log(cat.stomach);
